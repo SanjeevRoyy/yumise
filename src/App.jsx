0a7018eccc9d0cbe1ac6c297d628f42165c8af2a
SanjeevRoyy <abiralmanausya9@gmail.com>
2025-06-13 12:21:55 +0545

    setup tailwind and vite

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-red-500">Vite + React</h1>
+      <div className="p-10 space-y-6 ">
+      <h1 className="text-4xl text-[oklch(var(--color-avocado))]">
+        Avocado Green
+      </h1>
+      <h2 className="text-3xl text-[oklch(var(--color-sky))]">
+        Sky Blue
+      </h2>
+      <h3 className="text-2xl text-[oklch(var(--color-pink))]">
+        Pink Vibes
+      </h3>
+    </div>
+    </>
+  );
+}
+
+export default App;
